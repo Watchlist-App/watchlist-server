c45170c989a4bb169946926b30c7ede9ece9e902
Dmitry Mazuro <dmitry.mazuro@icloud.com>
2013-11-24 16:09:34 +0300

    Allowed CORS

--- config/routes.js
+++ config/routes.js
@@ -33,7 +33,8 @@ module.exports.routes = {
   // 
   // (This would also work if you had a file at: `/views/home.ejs`)
   '/': {
-    view: 'static/index'
+    view: 'static/index',
+    cors: true
   }
 
   /*
