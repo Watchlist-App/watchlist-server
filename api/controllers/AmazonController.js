c91f942b03375c6fbb32b40cc888d0aff1cce6c7
Dmitry Mazuro <dmitry.mazuro@icloud.com>
2013-12-02 16:40:39 +0300

    added amazon dvd search api

---
+++ api/controllers/AmazonController.js
@@ -0,0 +1,37 @@
+var util = require('util'),
+    OperationHelper = require('apac').OperationHelper;
+
+var awsId = process.env.AWS_ID;
+var awsSecret = process.env.AWS_SECRET;
+var awsTag = process.env.AWS_TAG;
+
+var opHelper = new OperationHelper({
+    awsId: awsId,
+    awsSecret: awsSecret,
+    assocId: awsTag
+});
+
+module.exports = {
+
+
+  'dvds': function(req, res){
+    var title = "";
+    title = req.param('title');
+
+    opHelper.execute('ItemSearch', {
+      'SearchIndex': 'DVD',
+      'Keywords': title,
+      'ResponseGroup': 'ItemAttributes,Offers, Images'
+    }, function(results) {
+      return res.send(results);
+    });
+
+  },
+
+  /**
+   * Overrides for the settings in `config/controllers.js`
+   * (specific to AmazonController)
+   */
+  _config: {}
+
+};
